6f919e016770c0548ffc7de90e510ba2bafbe96c
Kayk <ksm.kayk@outlook.com>
2022-02-09 13:42:55 -0300

    knex config and jest config

---
+++ knexfile.ts
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const user = process.env.DB_USER
+const password = process.env.DB_PASSWORD
+const host = process.env.DB_HOST
+const database = process.env.DB_DATABASE
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      host,
+      port : 5432,
+      user ,
+      password ,
+      database ,
+    },
+    migrations: {
+      tableName: "knexMigrations",
+      directory: `${__dirname}/src/database/migrations`
+    }
+  }
+};
