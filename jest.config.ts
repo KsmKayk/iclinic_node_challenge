b9bc3403b302582549011a4efca397f397805040
Kayk <ksm.kayk@outlook.com>
2022-02-09 16:30:32 -0300

    jest config reupload

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: [
+    "**/__tests__/**/*.test.ts?(x)",
+  ],
+  bail: true,
+  clearMocks: true,
+};
