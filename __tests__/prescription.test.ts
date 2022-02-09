d82e37e97fa585ad6b89eb81fabd06f28f60181a
Kayk <ksm.kayk@outlook.com>
2022-02-09 14:54:48 -0300

    changing export method

--- __tests__/prescription.test.ts
+++ __tests__/prescription.test.ts
@@ -1,9 +1,7 @@
+
+
 describe("Prescription", () => {
     it("should be able to create a new prescription", () => {
-        const x = 2;
-        const y = 4;
-        const sum = x + y;
 
-        expect(sum).toBe(6)
     })
 })
